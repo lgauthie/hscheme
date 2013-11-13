b8820e6ed0a77b0633eac47c40fae9451efbfdce
Lee Gauthier <lgauthie@uvic.ca>
2013-11-12 23:23:26 -0800

    Add if conditionals

--- Eval.hs
+++ Eval.hs
@@ -52,6 +52,11 @@ eval val@(Bool _) = return val
 eval val@(Char _) = return val
 eval val@(Atom _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", cond, conseq, alt]) =
+    do result <- eval cond
+       case result of
+            Bool False -> eval alt
+            _          -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 
 apply :: String -> [LispVal] -> ThrowsError LispVal

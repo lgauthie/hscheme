9338ded61147ad385f10799a0a7a0d76d4791d1d
Lee Gauthier <lgauthie@uvic.ca>
2013-11-26 15:31:02 -0800

    Change spacing in eval pattern matching

--- src/Eval.hs
+++ src/Eval.hs
@@ -39,15 +39,15 @@ eval env (List [Atom "if", cond, conseq, alt]) =
             notBool    -> throwError $ TypeMismatch "bool" notBool
 eval env (List [Atom "set!", Atom var, form]) = eval env form >>= setVar env var
 eval env (List [Atom "define", Atom var, form]) = eval env form >>= defineVar env var
-eval env (List (Atom "define" : List (Atom var : params') : body')) =
+eval env (List (Atom "define":List (Atom var:params'):body')) =
     makeNormalFunc env params' body' >>= defineVar env var
-eval env (List (Atom "define" : DottedList (Atom var : params') varargs : body')) =
+eval env (List (Atom "define":DottedList (Atom var:params') varargs:body')) =
     makeVarargs varargs env params' body' >>= defineVar env var
-eval env (List (Atom "lambda" : List params' : body')) =
+eval env (List (Atom "lambda":List params':body')) =
     makeNormalFunc env params' body'
-eval env (List (Atom "lambda" : DottedList params' varargs : body')) =
+eval env (List (Atom "lambda":DottedList params' varargs:body')) =
     makeVarargs varargs env params' body'
-eval env (List (Atom "lambda" : varargs@(Atom _) : body')) =
+eval env (List (Atom "lambda":varargs@(Atom _):body')) =
     makeVarargs varargs env [] body'
 eval env (List (function : args)) = do
     func <- eval env function
